8715a1d004d466554d1ed3c04eb040b0cb850360
Alan Palazzolo <apalazzolo@minnpost.com>
2015-06-20 15:01:50 -0500

    git add .git add .

--- src/signal.ino
+++ src/signal.ino
@@ -8,6 +8,12 @@
 #define PIN 6
 #define N_LEDS 64
 
+// 2 and 3 are interrupt PINs
+#define L_BUTTON 2
+#define L_LED 4
+#define R_BUTTON 3
+#define R_LED 5
+
 // Set up neopixel strip
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -21,8 +27,27 @@ uint32_t bg = strip.Color(255, 0, 0);
 // Blink delay
 int blink_delay = 400;
 
+// Debounce check time
+int debounce_time = 300;
+
+// State
+bool l_on = false;
+bool r_on = false;
+
 // Main setup function
 void setup() {
+
+  // Butttons
+  pinMode(L_BUTTON, INPUT);
+  pinMode(R_BUTTON, INPUT);
+
+  // Initialize LED
+  pinMode(L_LED, OUTPUT);
+  pinMode(R_LED, OUTPUT);
+
+  // Interrupt.  0 = PIN 2
+  attachInterrupt(0, on_button_left, FALLING);
+  attachInterrupt(1, on_button_right, FALLING);
 
   // Start NeoPixel strip
   strip.begin();
@@ -32,15 +57,61 @@ void setup() {
 
   // Background
   background();
+
+  // Serial
+  Serial.begin(9600);
 }
 
 // Main loop function
 void loop() {
+  background();
 
-  left(blink);
-  delay(blink_delay);
-  left(blink_off);
-  delay(blink_delay);
+  // Check if on
+  if (l_on) {
+    digitalWrite(L_LED, HIGH);
+    left(blink);
+    delay(blink_delay);
+
+    digitalWrite(L_LED, LOW);
+    left(blink_off);
+    delay(blink_delay);
+  }
+  else if (r_on) {
+    digitalWrite(R_LED, HIGH);
+    right(blink);
+    delay(blink_delay);
+
+    digitalWrite(R_LED, LOW);
+    right(blink_off);
+    delay(blink_delay);
+  }
+}
+
+
+// Handle interrupt
+static void on_button_left() {
+  // Debounce wrap
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounce_time) {
+    Serial.write('L');
+    l_on = !l_on;
+    r_on = false;
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+// Handle interrupt
+static void on_button_right() {
+  // Debounce wrap
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > debounce_time) {
+    Serial.write('R');
+    r_on = !r_on;
+    l_on = false;
+  }
+  last_interrupt_time = interrupt_time;
 }
 
 
